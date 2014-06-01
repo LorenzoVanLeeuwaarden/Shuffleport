207e1ddd26cdbd38bdec253ce5efe38e5df6926c
Hespen <petervanhespen@hotmail.com>
2014-06-01 14:56:52 +0200

    menu met muis (crappy)

--- Unity Project/Assets/Menu/PointerWithCursor.cs
+++ Unity Project/Assets/Menu/PointerWithCursor.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class PointerWithCursor : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        this.transform.position = new Vector3(Input.mousePosition.x/(Screen.width/2)-2,Input.mousePosition.y/(Screen.height/2)-1,0);
+        
 	}
 }
